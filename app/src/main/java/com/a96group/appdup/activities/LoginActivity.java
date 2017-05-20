a46b835990ebdd789970e1450dbddd7ae32bce40
sabeersulaiman <sabeersulaimanpv@gmail.com>
2017-05-20 11:04:20 +0530

    moved to latest gradle and removed Jack

--- app/src/main/java/com/a96group/appdup/activities/LoginActivity.java
+++ app/src/main/java/com/a96group/appdup/activities/LoginActivity.java
@@ -1,8 +1,12 @@
 package com.a96group.appdup.activities;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -32,6 +36,32 @@ public class LoginActivity extends AppCompatActivity {
             Log.d("Error : ", "Couldn't hide the Action bar.");
         }
         setContentView(R.layout.activity_login);
+
+        /*if (ContextCompat.checkSelfPermission(LoginActivity.this,
+                Manifest.permission.RECEIVE_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(LoginActivity.this,
+                    Manifest.permission.RECEIVE_SMS)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(LoginActivity.this,
+                        new String[]{Manifest.permission.RECEIVE_SMS},
+                        1);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }*/
 
         DigitsAuthButton digitsButton = (DigitsAuthButton) findViewById(R.id.auth_button);
         try
